e0256404eb2bada1c21078b8ea607b4fa2ecec6e
DavidJian <davidjian0905@gmail.com>
2018-03-26 16:29:53 +0800

    added endpoint for Ticker, OHLC, Orderbook for ETHUSD, BTCUSD, ETHBTC

--- lib/vhc_kraken/client.rb
+++ lib/vhc_kraken/client.rb
@@ -9,6 +9,55 @@ module VhcKraken
       response = connection.get 'Time'
       parse_response response
     end
+    #for BTCUSD
+    def self.get_OHLC_for_XBTUSD
+      response = connection.get "OHLC?pair=XBTUSD"
+      parse_response response
+    end
+
+    def self.get_ticker_for_XBTUSD
+      response = connection.get "Ticker?pair=XBTUSD"
+      parse_response response
+    end
+
+    def self.get_orderbook_for_XBTUSD
+      response = connection.get "Depth?pair=XBTUSD"
+      parse_response response
+    end
+
+    #for ETHUSD
+    def self.get_OHLC_for_ETHUSD
+      response = connection.get "OHLC?pair=ETHUSD"
+      parse_response response
+    end
+
+    def self.get_ticker_for_ETHUSD
+      response = connection.get "Ticker?pair=ETHUSD"
+      parse_response response
+    end
+
+    def self.get_orderbook_for_ETHUSD
+      response = connection.get "Depth?pair=ETHUSD"
+      parse_response response
+    end
+
+    #for ETHXBT
+    def self.get_OHLC_for_ETHXBT
+      response = connection.get "OHLC?pair=ETHXBT"
+      parse_response response
+    end
+
+    def self.get_ticker_for_ETHXBT
+      response = connection.get "Ticker?pair=ETHXBT"
+      parse_response response
+    end
+
+    def self.get_orderbook_for_ETHXBT
+      response = connection.get "Depth?pair=ETHXBT"
+      parse_response response
+    end
+
+
 
     class << self
       def connection
